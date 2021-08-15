20880e34fc5fa9f62100c614efd84802fdf7e64b
Raguggg <88898517+Raguggg@users.noreply.github.com>
2021-08-15 14:19:10 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -60,7 +60,7 @@ async def start(bot, update):
     # logger.info(update)
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(update.from_user.first_name),,
+        text=Translation.START_TEXT.format(update.from_user.first_name),
          reply_markup=InlineKeyboardMarkup([
         [InlineKeyboardButton("Telegram Sticker", url="https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTc2ODU0NzI4Mzk4MjY5?")],
         [InlineKeyboardButton(

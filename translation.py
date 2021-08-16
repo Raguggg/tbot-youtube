b5a0ec051e22e3f198bb7e1a6075f3b75f51b615
Raguggg <88898517+Raguggg@users.noreply.github.com>
2021-08-16 20:41:52 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -10,7 +10,7 @@ This is a FREE Telegram YouTube Downloader Bot!
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>Currently this option is unavailable,I am sorry😭😭</b>"
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> ."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     NOYES_URL = "raguutubebot and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
